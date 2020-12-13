fd83f95f9fd31418e99a5763846522222bc94ba6
jhoupps <j.c.houp7575@gmail.com>
2020-12-12 23:41:02 -0800

    rolled aws key

--- servers/gateway/deploy.sh
+++ servers/gateway/deploy.sh
@@ -2,9 +2,9 @@
 
 echo "not done yet!"
 
-#the command to ssh in is
+#the OLD command to ssh in is
 #ssh -i "../../comtalk_aws_instance.pem" ec2-user@ec2-34-211-151-171.us-west-2.compute.amazonaws.com
 
 ./build.sh 
 docker push comtalk/gateway
-ssh -i "../../comtalk_aws_instance.pem" ec2-user@ec2-34-211-151-171.us-west-2.compute.amazonaws.com < inside_aws_script.sh
+ssh -i "../../../comtalk_keys.pem" ec2-user@ec2-34-211-151-171.us-west-2.compute.amazonaws.com < inside_aws_script.sh
