09b42cf6da8c6e0bd6bb37a6c937e45ccd1483f8
jhoupps <j.c.houp7575@gmail.com>
2020-12-12 19:31:17 -0800

    added ssh commands to deploy file skeletons

---
+++ clients/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "not done yet!"
+
+#the command to ssh in is
+
+ssh -i "../comtalk_aws_instance.pem" ec2-user@ec2-18-236-237-131.us-west-2.compute.amazonaws.com
---
+++ servers/gateway/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "not done yet!"
+
+#the command to ssh in is
+
+ssh -i "../../comtalk_aws_instance.pem" ec2-user@ec2-34-211-151-171.us-west-2.compute.amazonaws.com
