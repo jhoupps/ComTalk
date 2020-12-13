47dc50033d8bbbd028616f9b593cb0c0020a147d
wanyuguan <wanyu20@uw.edu>
2020-12-12 18:19:29 -0800

    setup basic web client

---
+++ clients/src/Constants/Endpoints.js
@@ -0,0 +1,7 @@
+export default {
+    base: "",
+    testbase: "http://localhost", 
+    handlers: {
+        
+    }
+}
---
+++ clients/src/Constants/PageTypes.js
@@ -0,0 +1,5 @@
+export default {
+    signin: "Sign-In",
+    signup: "Signup",
+    
+}
