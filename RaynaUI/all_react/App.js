329f4d2316f8a7679a1c974f9a2c3d99943b2348
Rayna Tilley <raynat6@uw.edu>
2020-12-14 06:38:10 -0800

    experimenting and trying to render

--- RaynaUI/all_react/App.js
+++ RaynaUI/all_react/App.js
@@ -1,42 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
     constructor(props) {
 		super(props);
 		this.state = {
-			page: 'signin',
-            channels: ,
-            messages:
+			page: 'signin'//,
+            //channels: ,
+           // messages:
 		};
 
-		this.signInClick = this.signUpClick.bind(this);
-        this.signUpClick = this.signInClick.bind(this);
         this.signOutClick = this.signOutClick.bind(this);
-        this.homeClick = this.homeClick.bind(this);
+
         this.newForumClick = this.newForumClick.bind(this);
         this.joinForumClick = this.joinForumClick.bind(this);
+
+        this.homeClick = this.homeClick.bind(this);
         this.sendClick = this.sendClick.bind(this);
     }
-    
-    signInClick(event) {
 
-    }
-
-    signUpClick(event) {
-
-    }
-
+    // Landing & Forum
     signOutClick(event) {
 
     }
 
-    homeClick(event) {
-
-    }
-
+    // Landing
     newForumClick(event) {
 
     }
 
     joinForumClick(event) {
+
+    }
+
+     // Forum
+    homeClick(event) {
 
     }
 
@@ -48,10 +46,10 @@ class App extends React.Component {
         if (this.state.page == 'signin') {
             return(
                 <React.Fragment>
-                    <Signin />
+                    <SignIn />
                 </React.Fragment>
             );
-        } else if (this.stat.page == 'landing') {
+        } else if (this.state.page == 'landing') {
             return(
                 <React.Fragment>
                     <Landing />
@@ -70,6 +68,27 @@ class App extends React.Component {
 class SignIn extends React.Component {
     constructor(props) {
         this.state.sign = 'initial'
+
+        this.signInClick = this.signUpClick.bind(this);
+        this.signUpClick = this.signInClick.bind(this);
+        this.backClick = this.backClick.bind(this);
+        this.submitClick = this.submitClick.bind(this);
+    }
+
+    signUpClick(event) {
+        this.setState(sign = 'up')
+    }
+
+    signInClick(event) {
+        this.setState(sign = 'in')
+    }
+
+    backClick(event) {
+        this.setState(sign = 'initial')
+    }
+
+    submitClick(event) {
+        
     }
 
     render() {
@@ -79,19 +98,73 @@ class SignIn extends React.Component {
                 <div className="signin">
                     <div>
                         <label for="sign in"></label>
-                        <button aria-label="Sign In">Sign In</button>
+                        <button onClick={this.signInClick} aria-label="Sign In">Sign In</button>
                     </div>
                     <div>
                         <label for="sign up"></label>
-                        <button aria-label="Sign Up">Sign Up</button>
+                        <button onClick={this.signUpClick} aria-label="Sign Up">Sign Up</button>
                     </div>
                 </div>
                 </React.Fragment>
             );
         } else if (this.state.sign == 'up') {
-            return();
+            return(
+                <React.Fragment>
+                    <div>
+                        <h2>Sign Up</h2>
+                    </div>
+                    <div className="signup_form">
+                        <form>
+                            <div>
+                                <label for="email">Email:</label><br />
+                                <input type="text" id="email" /><br />
+                                <label for="username">Username:</label><br />
+                                <input type="text" id="username" /><br />
+                                <label for="fname">First Name:</label><br />
+                                <input type="text" id="fname" /><br />
+                                <label for="lname">Last Name:</label><br />
+                                <input type="text" id="lname" /><br />
+                                <label for="password">Password:</label><br />
+                                <input type="text" id="password" /><br />
+                                <label for="password_conf">Password Conformation:</label><br />
+                                <input type="text" id="password_conf" /><br />
+                            </div>
+                            <div>
+                                <label for="back"></label>
+                                <button onClick={this.backClick} aria-label="Back">Back</button>
+                                <label for="submit"></label>
+                                <button onClick={this.submitClick} aria-label="Submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </React.Fragment>
+            );
         } else if (this.state.sign == 'in') {
-            return();
+            return(
+                <React.Fragment>
+                    <div>
+                        <h2>Sign In</h2>
+                    </div>
+                    <div className="signin_form">
+                        <form>
+                            <div>
+                                <label for="email">Email:</label><br />
+                                <input type="text" id="email" /><br />
+                                <label for="password">Password:</label><br />
+                                <input type="text" id="password" /><br />
+                            </div>
+                            <div>
+                                <label for="back"></label>
+                                <button onClick={this.backClick} aria-label="Back">Back</button>
+                                <label for="submit"></label>
+                                <button onClick={this.submitClick} aria-label="Submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </React.Fragment>
+            );
         }
     }
 }
+
+ReactDOM.render(<App />, document.getElementById('insert_data'));
